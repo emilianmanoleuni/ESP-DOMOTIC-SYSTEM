a92a2897bc5dbac73366a64f17409ccb63326d87
leorigo2 <112388548+leorigo2@users.noreply.github.com>
2024-02-21 14:56:18 +0100

    removed obsolete code

--- esp_main/esp_main_code/esp_uart.h
+++ esp_main/esp_main_code/esp_uart.h
@@ -2,11 +2,9 @@
 #define __ESP_UART_H__
 
 #include <HardwareSerial.h>
-//#include "fan.h"
 
 void UARTinit();
 int  getData();
-void handleData();
 void sendInterrupt(int data);
 float readTemperature();
 int readLight();
